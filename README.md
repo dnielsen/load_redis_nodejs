dcbfa9c903e4e53f21d13a974a3db10e4278d2a9
Ahmed <ahmedcs2012@gmail.com>
2018-07-03 01:58:22 +0200

    finish task

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # load_redis_nodejs
 load data into redis with node.js
+
+
+to run this demo
+
+# npm install
+# npm start 
--- load.js
+++ load.js
@@ -2,23 +2,40 @@ var redis = require('redis');
 var client = redis.createClient(); //creates a new client. Defaults to 127.0.0.1 and 6379
 // var client = redis.createClient("127.0.0.1", 6379); // specify your own ip address and port
 
-console.log('connected to redis');
+
+// import build in 'path' module , helper module in path manipulation and concatention
+var path = require('path');
 
 
+// Load FileSystem built in Module to can read from hard disk
 var fs = require('fs');
-var parse = require('csv-parse');
-var fileName = "questions.csv"; 
 
-var text = fs.readFileSync(fileName, "utf8");
-
-console.log('connected to fs');
+// path.join it like  DIRECTORY_SEPERATOR in PHP Langauge
+var fileName = path.join(__dirname,"questions.csv");
 
 
-    text.split(/\r?\n/).forEach(function (line) {
-        // ...
-        client.hmset('key1', 'f1', 'value1', 'f2', 'value2', 'f3', 'value3');
-        // ...
-    });
+var csv = require('csv-parser')
+let arrayOfRows = [];
+
+// Read From Hard Disk File and PIPE output to CSV lib to can convert CSV data into JAVASCRIPT Object
+fs.createReadStream(fileName)
+  .pipe(csv())
+  // this event with name 'data' will be fired for each row in CSV
+  .on('data', function (row) {
+    arrayOfRows.push(row);
+    // this event will be fired when complete read and convert csv file into javascript data.
+  }).on('end',function(){
+    // Convert arrayOfRows into JSON formate using JSON.stringify Method
+    // Store json data into Redis in Key with name 'csv_data'
+    if(arrayOfRows.length){
+      client.set("csv_data",JSON.stringify(arrayOfRows), redis.print);
+    }
 
 
-console.log('data import complete');
+    /* Test Reading data from Redis and Convert JSON into Javascript array */
+      client.get('csv_data',(err,data)=>{
+        // Read Data from Redis as JSON string and convert it into Javascript Array using JSON.parse
+        let arrayOfRows = JSON.parse(data);
+        console.log(arrayOfRows);
+      });
+  })
